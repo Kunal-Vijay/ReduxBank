60ae3967b5c529cf00b4e63d7da23708ac59335c
Kunal <kunal20vijay00@gmail.com>
2022-06-11 23:50:03 +0530

    updating state from bank component using bindActionCreators

--- src/Bank.js
+++ src/Bank.js
@@ -1,15 +1,18 @@
 import React from 'react'
 import { useDispatch } from 'react-redux'
+import { bindActionCreators } from 'redux'
 import actionCreators from './state/index'
+
 const Bank = () => {
     const dispatch = useDispatch();
+    const { withdrawMoney, depositMoney } = bindActionCreators(actionCreators, dispatch);
     return (
         <div>
             <h2 style={{ margin: "20px" }}>Deposit/Withdraw money</h2>
-            <button className="btn btn-primary"onClick={()=>{dispatch(actionCreators.withdrawMoney(100))}} >-</button>
+            <button className="btn btn-primary"onClick={()=>withdrawMoney(100)} >-</button>
             <span style={{ margin: "10px" }}>Update</span>
-            <button className="btn btn-primary" onClick={()=>{dispatch(actionCreators.depositMoney(100))}}>+</button>
-        </div>
+            <button className="btn btn-primary" onClick={()=>depositMoney(100)}>+</button>
+        </div >
     )
 }
 
